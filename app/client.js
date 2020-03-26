697d5d4e3bad38412ad431e55376f7baf5d4b9f8
Travis Haby <travishaby@gmail.com>
2020-03-25 21:12:22 -0600

    Clear out all that ol csv junk and old data source stuffs, add new json data source

--- app/client.js
+++ app/client.js
@@ -1,20 +1,8 @@
-const csvsUrl =
-  "https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports";
+const dataUrl = "https://coronadatascraper.com/timeseries-byLocation.json"
 
-const csvFileRegex = new RegExp(/\d{2}-\d{2}-\d{4}\.csv/);
-
-const getHopkinsCSVs = () =>
-  fetch(csvsUrl)
+const getData = () =>
+  fetch(dataUrl)
     .then(response => response.json())
-    .then(result => {
-      return result
-        .filter(item => {
-          const notJan = item.name.indexOf("01") != 0;
-          const notFeb = item.name.indexOf("02") != 0;
-          return notJan && notFeb && item.name.match(csvFileRegex);
-        })
-        .map(item => item.download_url)
-    })
     .catch(console.alert);
 
 module.exports = getHopkinsCSVs;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,78 +1,31 @@
 const React = require("react");
-const parse = require('csv-parse/lib/sync')
 
 const LineChart = require('./LineChart')
-const Table = require('./Table')
 const Loading = require('./Loading')
 const {DataContext} = require('../DataContext')
 const {SET_STATES_BY_DATE} = require('../DataReducer')
-const getHopkinsCsvs = require("../client");
+const getData = require("../client");
 const {states, topStates} = require('../utils')
-const fixtureData = require('../fixtureData')
+
 
 const Main = function() {
   const [csvLocations, setCsvLocations] = React.useState([])
   const [stateName, setStateName] = React.useState(topStates[Math.floor(Math.random() * topStates.length)]) // default to random from top states
   const { state, dispatch } = React.useContext(DataContext)
 
-  // on compnent mount
-  React.useEffect(() => {
-    getHopkinsCsvs()
-      .then(response => setCsvLocations(response))
-      .catch(console.error)
-  }, [])
-
-  React.useEffect(() => {
-    // Don't run if the locations array is empty
-    if (csvLocations.length !== 0) {
-      csvLocations.forEach(location => {
-        const pathParts = location.split('/')
-        const date = pathParts[pathParts.length - 1].split('.')[0]
-        fetch(location)
-          .then(res => res.blob())
-          .then(blob => blob.text())
-          .then(text => parse(text, {columns: true}))
-          .then(csv => csv.reduce((agg, row) => {
-            if (row["Country/Region"] === "US") agg[row["Province/State"]] = row
-            return agg
-          }, {}))
-          .then(data => {
-            dispatch({ type: SET_STATES_BY_DATE, payload: { [date]: data } })
-          })
-          .catch(console.error)
-        })
-    }
-  }, [csvLocations])
-
-  const dateKeys = Object.keys(state.statesByDate)
-  const dates = dateKeys.sort((a, b) => {
-    return new Date(a.replace('-', ' ')) - new Date(b.replace('-', ' '))
-  })
-  const allStatesOrdered = dates.map(date => {
-    return { ...state.statesByDate[date], date }
-  })
-  const data = allStatesOrdered.reduce((agg, datum) => {
-    const stateData = datum[stateName]
-    if (stateData) agg.push({ ...stateData, date: datum.date })
-    return agg
-  }, [])
-  const fullyLoaded = (dateKeys.length !== 0) && (dateKeys.length === csvLocations.length)
   const handleSelect = event => setStateName(event.target.value)
   return (
-    <div class="container">
-      <div class="row">
+    <div className="container">
+      <div className="row">
         <h1>Cumulative COVID-19 cases by US State</h1>
       </div>
-      <div class="row">
+      <div className="row">
         <p>
-          This is an app to visualize the COVID trends in a given state. Data is
-          sourced from Johns Hopkins, specifically the csvs that are uploaded
-          <a href="https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports">
-          &nbsp;here
-          </a>
+          This is an app to visualize the COVID trends in a given US state. Data is
+          sourced from <a href="https://coronadatascraper.com">Corona Data Scraper</a>
         </p>
       </div>
-      <div class="row">
+      <div className="row">
         <div>
           Choose a state from the dropdown&nbsp;
           <select name="choice" onChange={handleSelect} defaultValue={stateName}>
@@ -90,13 +43,6 @@ const Main = function() {
 
         {fullyLoaded ? (
           <LineChart data={data} stateName={stateName} />
-        ) : (
-          <Loading />
-        )}
-      </div>
-      <div class="row align-items-center mt-5 mb-5">
-        {fullyLoaded ? (
-          <Table data={allStatesOrdered} />
         ) : (
           <Loading />
         )}
